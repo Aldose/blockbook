926d47c7f5fadb51949029ada8c0887836af3d92
Alex Frankish <alexfrankish@gmail.com>
2018-03-11 17:40:50 +0800

    added python reading test

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,6 +4,15 @@ var path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.resolve('views/index.html'))
+  var sys   = require('util'),
+    spawn = require('child_process').spawn,
+    dummy  = spawn('python', [path.resolve('python/test.py')]);
+
+    dummy.stdout.on('data', function(data) {
+      result = data.toString();
+        // console.log(data.toString());
+        console.log(result[0])
+    });
 });
 
 oliverClosesOff = 'WalletID';
