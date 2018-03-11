457c3b38324ed69badd0057032ce344712ba2bc1
Alex Frankish <alexfrankish@gmail.com>
2018-03-11 16:54:18 +0800

    small changes

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.resolve('views/index.html'))
 });
+
+oliverClosesOff = 'WalletID';
+smartContractAddress = 'SmartContract Address';
+
 router.post('/', (req,res,next)=>{
   // connect to the API
   // update contract
